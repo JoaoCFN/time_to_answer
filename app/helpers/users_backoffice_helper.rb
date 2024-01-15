77d5d5e3def0900eb2623d30dbb120bfce69e428
João Carlos Ferreira Neto <58020444+JoaoCFN@users.noreply.github.com>
2024-01-15 16:28:19 -0300

    feat: creating active storage upload to user profile

--- app/helpers/users_backoffice_helper.rb
+++ app/helpers/users_backoffice_helper.rb
@@ -3,4 +3,9 @@ module UsersBackofficeHelper
     current_user_full_name = current_user.full_name
     current_user_full_name.present? ? "Seja bem-vindo #{current_user_full_name}" : "Seja bem-vindo"
   end
+
+  def avatar_url
+    avatar = current_user.user_profile.avatar
+    avatar.attached? ? avatar : 'img.jpg'
+  end
 end
