6fdc6cd277e10738ec9db1aad82616a4d818cfb1
João Carlos Ferreira Neto <58020444+JoaoCFN@users.noreply.github.com>
2024-01-31 20:14:33 -0300

    refactor: improve redis manipulation in answer controller

--- app/controllers/site/answer_controller.rb
+++ app/controllers/site/answer_controller.rb
@@ -1,19 +1,27 @@
 class Site::AnswerController < SiteController
   def question
-    redis_answer = Rails.cache.fetch(params[:answer_id])
+    @redis_answer = Rails.cache.fetch(params[:answer_id])
+    @redis_answer.present? ? set_redis_answer : set_database_answer
 
-    if redis_answer.present?
-      splited_redis_answer = redis_answer.split("@@")
+    UserStatistic.set_statistic(@answer_is_correct, current_user)
+  end
+
+  private
+    def to_bool(string)
+      ActiveModel::Type::Boolean.new.cast(string)
+    end
+
+    def set_redis_answer
+      splited_redis_answer = @redis_answer.split("@@")
 
       @question_id = splited_redis_answer.first
-      @answer_is_correct = ActiveModel::Type::Boolean.new.cast(splited_redis_answer.last)
-    else
+      @answer_is_correct = to_bool(splited_redis_answer.last)
+    end
+
+    def set_database_answer
       answer = Answer.find(params[:answer_id])
 
       @question_id = answer.question_id
       @answer_is_correct = answer.correct
     end
-
-    UserStatistic.set_statistic(@answer_is_correct, current_user)
-  end
 end
