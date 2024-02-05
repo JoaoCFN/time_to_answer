25f671c0d84493c6e0d6a162a02b18fb5cbe2d99
João Carlos Ferreira Neto <58020444+JoaoCFN@users.noreply.github.com>
2024-02-05 18:15:26 -0300

    feat: change the ordination in questions controller

--- app/controllers/admins_backoffice/questions_controller.rb
+++ app/controllers/admins_backoffice/questions_controller.rb
@@ -1,30 +1,30 @@
 class AdminsBackoffice::QuestionsController < AdminsBackofficeController
     before_action :set_question, only: [:edit, :update, :destroy]
     before_action :get_subjects, only: [:new, :edit]
-  
+
     def index
       @questions = Question.includes(:subject)
-                           .order(:description)
+                           .order(:id)
                            .page(params[:page])
     end
-  
+
     def new
       @question = Question.new
     end
-  
+
     def create
       @question = Question.new(params_question)
-  
+
       if(@question.save())
         redirect_to admins_backoffice_questions_path, notice: "Questão criada com sucesso"
       else
         render :edit
       end
     end
-  
+
     def edit
     end
-    
+
     def update
       if(@question.update(params_question))
         redirect_to admins_backoffice_questions_path, notice: "Questão atualizada com sucesso"
@@ -32,7 +32,7 @@ class AdminsBackoffice::QuestionsController < AdminsBackofficeController
         render :edit
       end
     end
-  
+
     def destroy
       if(@question.destroy())
         redirect_to admins_backoffice_questions_path, notice: "Questão excluida com sucesso"
@@ -40,12 +40,12 @@ class AdminsBackoffice::QuestionsController < AdminsBackofficeController
         render :index
       end
     end
-    
+
     private
       def params_question
         params.require(:question).permit(:description, :subject_id, answers_attributes: [:id, :description, :correct, :_destroy])
       end
-  
+
       def set_question
         @question = Question.find(params[:id])
       end
@@ -54,4 +54,4 @@ class AdminsBackoffice::QuestionsController < AdminsBackofficeController
         @subjects = Subject.all
       end
   end
-  
+
